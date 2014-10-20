bc8930c94b9e6bf1961a5aff6d94be896c93a678
Gebegebe <kosminska.p@gmail.com>
2014-10-20 12:50:50 +0200

    [#4]article

---
+++ article.rb
@@ -0,0 +1,43 @@
+class Article
+  
+  def initialize(title, content, author)
+    @title, @content, @author = title, content, author
+    @created_at = Time.now
+    @likes = @dislikes = 0
+  end
+   
+  def like!
+    @likes += 1
+  end
+  
+  def dislike!
+    @dislikes += 1
+  end
+  
+  def positive_votes
+    @likes - @dislikes
+  end
+  
+  def votes
+    @likes + @dislikes
+  end
+  
+  def shortened_to(limit)
+    if limit > @content.length
+      @content	
+    end
+    @content[0..limit-3] + "..."
+  end
+  
+  def include?(elem)
+    @content.include?(elem)
+  end
+  
+  def words
+    @content.split(/\W+/)
+  end
+  
+  def distinct_words
+    words.uniq
+  end
+end
